17140f678d88646c5d4e78355596244387dff2c4
John- Akachi Daniel <Akachidanil@gmail.com>
2018-02-01 05:43:58 +0100

    Add files via upload

---
+++ src/number/generator/NumberGenerator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package number.generator;
+
+/**
+ *
+ * @author Dell E6540
+ */
+public class NumberGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Generator g=new Generator();
+        g.setSize(500,500);
+        g.setVisible(true);
+    }
+    
+}
